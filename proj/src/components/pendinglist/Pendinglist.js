286d782d7810a3cbd90310326046f102b03633a2
liu wen chao <15267003387@139.com>
2018-07-19 11:03:28 +0800

    modify

--- proj/src/components/pendinglist/Pendinglist.js
+++ proj/src/components/pendinglist/Pendinglist.js
@@ -1,50 +1,55 @@
 require('./Pendinglist.styl');
 const Listitem = require('components/listitem');
-const Myslide = require('components/myslide');
 import classNames from 'classnames';
-import {Toast, Button, ScrollList, Context} from 'saltui'
+import { ScrollList } from 'saltui'
 class Pendinglist extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-        };  
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        this.props.handleCancelReplay && this.props.handleCancelReplay();
     }
     render() {
         if (!this.props.leftSlip) {
-            var RefreshItem = <Listitem {...this.props} refreshing={this.props.refreshing} />;
-        } else {
-            var RefreshItem = <Myslide {...this.props} refreshing={this.state.refreshing} />;
+            var RefreshItem = <Listitem
+                {...this.props}
+                refreshing={this.props.refreshing}
+            />;
         }
         return (
-            <div className={classNames('pendinglist')}>
-                <div className={classNames("container", { "detail-height": this.props.norefresh })}>
-                    {!this.props.norefresh ?
-                        <ScrollList
-                            dataGetted={this.props.dataGetted}
-                            data={this.props.data}
-                            hasError={this.props.hasError}
-                            refreshing={this.props.refreshing}
-                            onRefresh={this.props.onRefresh}
-                            loading={this.props.loading}
-                            onLoad={this.props.onLoad}
-                            noMore={this.props.noMore}
-                            desMaxLine={14}
-                        >
-                            {RefreshItem}
-                        </ScrollList> :
-                        (this.props.data && this.props.data.length > 0 && this.props.data.map((item, index) => {
-                            return (
-                                <Listitem
-                                    {...this.props}
-                                    select = {false}
-                                    ref={`listItem${index}`}
-                                    data={item}
-                                    key={index}
-                                />);
-                        }))}
-                </div>
+            // <div className={classNames('pendinglist')}></div>
+            <div className={classNames("pendinglist", { "detail-height": this.props.norefresh })} onClick={this.props.type != "news" && this.props.type != "card" && this.handleClick}>
+                {!this.props.norefresh ?
+                    <ScrollList
+                        dataGetted={this.props.dataGetted}
+                        data={this.props.data}
+                        hasError={this.props.hasError}
+                        refreshing={this.props.refreshing}
+                        onRefresh={this.props.onRefresh}
+                        loading={this.props.loading}
+                        onLoad={this.props.onLoad}
+                        noMore={this.props.noMore}
+                        desMaxLine={14}
+                        scrollRefresh={!this.props.noscrollRefresh}
+                    >
+                        {RefreshItem}
+                    </ScrollList> :
+                    (this.props.data && this.props.data.length > 0 && this.props.data.map((item, index) => {
+                        return (
+                            <Listitem
+                                {...this.props}
+                                select={false}
+                                ref={`listItem${index}`}
+                                data={item}
+                                key={index}
+                            />);
+                    }))}
             </div>
+
         );
     }
 
